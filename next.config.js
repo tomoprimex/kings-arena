3a1e52b3b01f9c9e1d8b69b5f40966b488574fa6
DIGITEC TRAINING INSTITUTE <digitecfirm@gmail.com>
2026-05-29 10:40:23 +0100

    fix next config module error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
